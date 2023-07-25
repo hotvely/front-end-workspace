b892afe25e33639cf3b7c8adcf2b9356538bb424
hotvely <hwan_o3o@naver.com>
2023-07-25 16:48:01 +0900

    2023_07_25
    javascript 정규식./

--- 3_JavaScript/js/실습문제2_이벤트.js
+++ 3_JavaScript/js/실습문제2_이벤트.js
@@ -28,3 +28,80 @@ window.addEventListener('scroll', function(){
 
 window.addEventListener('resize', resizeHandler);
 resizeHandler();
+
+// navigation click event
+const nav = document.querySelector('nav');
+let selectedHead = null;
+function navHandler(event)
+{
+    if(event.target == nav)
+        return;
+    if(selectedHead != null)
+    {
+        selectedHead.style.backgroundColor = 'transparent';
+        selectedHead.style.color = 'black';
+        selectedHead = null;
+    }
+
+    if(event.target !== event.currTarget)
+    {
+        console.log(event.target);
+        event.target.style.backgroundColor = 'black';
+        event.target.style.color = 'white';
+        selectedHead = event.target;
+    }
+ 
+}
+
+nav.addEventListener('click', navHandler);
+
+const section1 = document.querySelector('#section1');
+const section2 = document.querySelector('#section2');
+const section3 = document.querySelector('#section3');
+
+let currentSection = section1;
+
+window.addEventListener('wheel', function(event){
+    if(event.deltaY > 0)        // 휠 아래
+    {
+        if(currentSection == section1)
+        {
+            window.scrollTo({top: section2.offsetTop, behavior:'smooth'});
+            currentSection = section2;
+            console.log(section2.offsetTop);
+        }
+        else if(currentSection === section2)
+        {
+            window.scrollTo({top: section3.offsetTop, behavior:'smooth'});
+            currentSection = section3;
+        }
+    }
+    else        // 휠 위로
+    {
+        if(currentSection === section3)
+        {
+            window.scrollTo({top: section2.offsetTop, behavior:'smooth'});
+            currentSection = section3;
+        }
+        else if( currentSection == section2)
+        {
+            window.scrollTo({top: section1.offsetTop, behavior:'smooth'});
+            currentSection = section3;
+        }
+    }
+
+});
+
+// scroll - animation
+// window.addEventListener('scroll'
+// {
+//     if(section2.getBoundingClientRect().top == 0)
+//     {
+//         section2.children[0].classList.add('text-animation');
+//     }
+//     else
+//     {
+
+//         section2.children[0]/class
+//     }
+// };
